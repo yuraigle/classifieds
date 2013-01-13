ec9b04ef89027d5118e1813dd581e0ba603d4938
Rene Aigle <reneaigle@gmail.com>
2013-01-14 01:55:17 +0900

    ...

--- doctrine/Entity/Country.php
+++ doctrine/Entity/Country.php
@@ -26,4 +26,60 @@ class Country extends Core
      * @ORM\Column(type="string")
      */
     protected $code;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Country
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Country
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
 }
