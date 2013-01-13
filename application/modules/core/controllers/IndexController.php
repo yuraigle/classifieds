c2c6e5787280ab48d06723832813801233321fff
Rene Aigle <reneaigle@gmail.com>
2013-01-13 21:18:00 +0900

    1

--- application/modules/core/controllers/IndexController.php
+++ application/modules/core/controllers/IndexController.php
@@ -10,5 +10,19 @@ class Core_IndexController extends Bisna\Controller\Action
     public function indexAction()
     {
         // action body
+        $request = array(
+            "username" => "Rene",
+            "password" => "qwerty",
+            "email" => "rene@gmail.com",
+        );
+
+        $user = new \Entity\User();
+        // $user->populate($request);
+        $user->setUsername("Rene");
+        $user->setPassword("qwerty");
+        $user->setEmail("rene@gmail.com");
+
+        $this->em()->persist($user);
+        $this->em()->flush();
     }
 }
